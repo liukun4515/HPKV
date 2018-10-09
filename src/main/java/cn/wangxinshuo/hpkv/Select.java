2a7bbffdf1e3f23cf9cb6add6780c6e0915ccf67
wangxinshuo <18862616362@163.com>
2018-10-09 15:11:17 +0800

    完成查找部分，第一阶段以及完成，后面将进行优化。

--- src/main/java/cn/wangxinshuo/hpkv/Select.java
+++ src/main/java/cn/wangxinshuo/hpkv/Select.java
@@ -1,35 +1,28 @@
 package cn.wangxinshuo.hpkv;
 
 import cn.wangxinshuo.hpkv.file.CreateFileResources;
-import cn.wangxinshuo.hpkv.key.KeyDataType;
 import cn.wangxinshuo.hpkv.util.ByteArrayToLong;
-import cn.wangxinshuo.hpkv.value.KeyValueDataType;
 import com.google.common.primitives.UnsignedLong;
+import org.apache.commons.lang3.SerializationUtils;
 
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.util.HashMap;
 
 /**
  * @author wszgr
  */
 public class Select {
     private CreateFileResources resources;
-    private ArrayList<KeyDataType> list1;
-    private ArrayList<KeyValueDataType> list2;
     private final int NUMBER_OF_FILES = 1024;
 
     public Select(CreateFileResources resources) {
         this.resources = resources;
-        list1 = new ArrayList<KeyDataType>(NUMBER_OF_FILES);
-        list2 = new ArrayList<KeyValueDataType>(NUMBER_OF_FILES / 4);
-    }
-
-    private void loadKeyCache() {
-
     }
 
     public byte[] get(byte[] inKey) {
         UnsignedLong key = ByteArrayToLong.getKey(inKey);
-
-        return null;
+        InputStream stream = resources.getKeyResources(key, false);
+        HashMap<UnsignedLong, byte[]> map = SerializationUtils.deserialize(stream);
+        return map.get(key);
     }
 }
