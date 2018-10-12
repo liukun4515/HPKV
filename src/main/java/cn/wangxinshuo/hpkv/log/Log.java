000bb273db54c35a62412dde8ff2422e7937a72f
wangxinshuo <18862616362@163.com>
2018-10-13 00:28:46 +0800

    store:311492 ms

--- src/main/java/cn/wangxinshuo/hpkv/log/Log.java
+++ src/main/java/cn/wangxinshuo/hpkv/log/Log.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 public class Log {
     private String logPath;
     private String mode;
-    private final int KV_NUMBER = 1024;
+    private final int KV_NUMBER = 4096;
     private volatile RandomAccessFile randomAccessFile;
 
     public Log(String path) throws EngineException {
